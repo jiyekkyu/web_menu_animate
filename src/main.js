d608708ff54aa18a924e9b4dca14f19f3cfae5f9
leeji5670 <leeji3063@naver.com>
2021-06-24 16:11:06 +0900

    css folder path modify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
-import "@/css/animate.css";
+import "@/assets/css/animate.css";
 
 createApp(App).use(router).mount("#app");
